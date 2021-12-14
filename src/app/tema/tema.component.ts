0b4cf21df9704225ef2fa5eb8068346ac2bf8565
Pajtak <brunofpajtak@gmail.com>
2021-12-13 22:34:43 -0300

    Get e Post de Temas v1.0

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
+import { TemaService } from './../service/tema.service';
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment.prod';
 import { Router } from '@angular/router';
+import { Tema } from '../model/tema';
 
 @Component({
   selector: 'app-tema',
@@ -11,15 +13,20 @@ export class TemaComponent implements OnInit {
   nome = environment.nome
   foto = environment.foto
   constructor(
-    private router: Router
+    private router: Router,
+    private temaService: TemaService
   ) { }
 
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
 
   ngOnInit(){
     if(environment.token == ''){
       alert('Sua sessão expirou, faça o login novamente!')
       this.router.navigate(['/login'])
     }
+
+    this.findAllTemas()
   }
   sair(){
     this.router.navigate(['/login'])
@@ -27,6 +34,20 @@ export class TemaComponent implements OnInit {
     environment.nome = ''
     environment.foto = ''
     environment.id = 0
+  }
+
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[])=> {
+      this.listaTemas = resp
+    })
+  }
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema) => {
+      this.tema = resp
+      alert('Tema cadastrado com sucesso!')
+      this.findAllTemas()
+      this.tema = new Tema()
+    })
   }
 
   }
